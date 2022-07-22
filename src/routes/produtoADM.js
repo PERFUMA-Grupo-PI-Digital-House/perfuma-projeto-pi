7934daa3980f46366ae936d17d6640126c90d3f6
Gabriel Oliveira <gabrieloliveirasantos196@gmail.com>
2022-07-22 18:24:39 -0300

    atualização de um erro de codigo e tirando alguns  html que já foram convertidos em ejs

--- src/routes/produtoADM.js
+++ src/routes/produtoADM.js
@@ -1,11 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
-<<<<<<< HEAD
+
 const produtoADMController = require("../controllers/produtoADMController");
-=======
-const produtoADMController = require("../controllers/ProdutoADMController");
->>>>>>> 9f5550f734261aa99615cd86f275ea2178a6d44c
+
 
 
 router.get("/", produtoADMController.produtoADM);
